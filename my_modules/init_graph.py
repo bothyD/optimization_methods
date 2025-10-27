4da5635638b7b7f4963cbc469cdfed7127aee326
bothyD <pavla.acc@mail.ru>
2025-10-27 22:05:22 +0700

    some edit init

--- my_modules/init_graph.py
+++ my_modules/init_graph.py
@@ -2,19 +2,23 @@ from typing import List, Tuple
 from my_modules.Edge import Edge
 
 def init_grpah() -> Tuple[int, int, int, List[Edge]]:
-    peaks: int = 5 
-    edges: int = 7  
+  
     start_peack: int = 0  
-    graph: List[Edge] = [
-        # example 1 from book
-        # Edge(0, 1, 25),
-        # Edge(0, 2, 15),
-        # Edge(0, 3, 7),
-        # Edge(0, 4, 2),
-        # Edge(1, 2, 6),
-        # Edge(2, 3, 4),
-        # Edge(3, 4, 3)
+    # peaks: int = 5 
+    # edges: int = 7 
+    # graph: List[Edge] = [
+    #     # example 1 from book
+    #     Edge(0, 1, 25),
+    #     Edge(0, 2, 15),
+    #     Edge(0, 3, 7),
+    #     Edge(0, 4, 2),
+    #     Edge(1, 2, 6),
+    #     Edge(2, 3, 4),
+    #     Edge(3, 4, 3)
 
+    peaks: int = 6 
+    edges: int = 9 
+    graph: List[Edge] = [
         # example 2 from book
         Edge(0, 1, 13),
         Edge(0, 2, 18),
