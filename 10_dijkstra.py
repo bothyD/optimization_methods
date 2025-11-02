8f10d71bc8aa67f740c96d05ca4777567ee4361e
bothyD <pavla.acc@mail.ru>
2025-11-02 18:24:03 +0700

    edit output dijkstra

--- 10_dijkstra.py
+++ 10_dijkstra.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from typing import List, Tuple
 from my_modules.draw_graph import draw_graph
 from my_modules.init_graph import init_grpah
@@ -7,6 +8,30 @@ from my_modules.get_path import get_path
 from my_modules.Edge import Edge
 
 INF = np.inf
+
+
+# глобальный список шагов
+steps_data = []
+def add_step(visited: list, current_point: int, dist_cur_point: int, dist: list):
+    """Добавляет строку в общую таблицу шагов."""
+    visited_nodes = [i for i, v in enumerate(visited) if v]
+    row = {
+        "S": str(visited_nodes),  # превращаем список в строку, чтобы красиво печатался
+        "w": current_point,
+        "D(w)": dist_cur_point,
+    }
+    for i, d in enumerate(dist):
+        if visited[i+1] == True:
+            row[f"D({i+1})"] = "-"
+        else:
+            row[f"D({i+1})"] = d  # сдвиг на +1, чтобы совпадало с твоим выводом D(1), D(2), ...
+    steps_data.append(row)
+
+def print_table():
+    """Выводит все накопленные шаги одной таблицей."""
+    df = pd.DataFrame(steps_data)
+    print("\nРезультаты шагов алгоритма:\n")
+    print(df.to_string(index=False))
 
 
 def solve_dense(n: int, m: int, v: int, edges: List[Edge], undirected: bool = True) -> Tuple[List[float], List[int], int]:
@@ -25,6 +50,7 @@ def solve_dense(n: int, m: int, v: int, edges: List[Edge], undirected: bool = Tr
             if not visited[i] and distance[i] < min_dist:
                 min_dist = distance[i]
                 u = i    
+        
 
         if u == -1: 
             break  
@@ -38,8 +64,8 @@ def solve_dense(n: int, m: int, v: int, edges: List[Edge], undirected: bool = Tr
                 distance[neighbor] = distance[u] + cost
                 prev[neighbor] = u
                 operation_count += 1
-
-        print("Текущие расстояния:", distance)
+        add_step(visited, u, min_dist, distance[1:])
+        print_table()
 
     return distance, prev, operation_count
 
@@ -51,13 +77,12 @@ def main():
     for t in range(peaks):
         print(f"Кратчайший путь до вершины {t}: {get_path(prev, t)}, расстояние = {distance[t]}")
 
-
     theoretical_complexity = f"O(V²) = O({peaks}²)"
     theoretical_complexity_res = peaks ** 2
     print(f"\nТеоретическая трудоёмкость: {theoretical_complexity} = {theoretical_complexity_res} операций")
     print(f"Практическая трудоёмкость: {practical_ops} элементарных операций")
 
-    draw_graph(graph)
+    # draw_graph(graph)
 
 
 if __name__ == '__main__':
--- my_modules/init_graph.py
+++ my_modules/init_graph.py
@@ -4,31 +4,31 @@ from my_modules.Edge import Edge
 def init_grpah() -> Tuple[int, int, int, List[Edge]]:
   
     start_peack: int = 0  
-    # peaks: int = 5 
-    # edges: int = 7 
-    # graph: List[Edge] = [
-    #     # example 1 from book
-    #     Edge(0, 1, 25),
-    #     Edge(0, 2, 15),
-    #     Edge(0, 3, 7),
-    #     Edge(0, 4, 2),
-    #     Edge(1, 2, 6),
-    #     Edge(2, 3, 4),
-    #     Edge(3, 4, 3)
-
-    peaks: int = 6 
-    edges: int = 9 
+    peaks: int = 5 
+    edges: int = 7 
     graph: List[Edge] = [
-        # example 2 from book
-        Edge(0, 1, 13),
-        Edge(0, 2, 18),
-        Edge(0, 3, 17),
-        Edge(0, 4, 14),
-        Edge(0, 5, 22),
-        Edge(1, 2, 26),
-        Edge(1, 4, 22),
-        Edge(2, 3, 3),
-        Edge(3, 5, 19)
-
+        # example 1 from book
+        Edge(0, 1, 25),
+        Edge(0, 2, 15),
+        Edge(0, 3, 7),
+        Edge(0, 4, 2),
+        Edge(1, 2, 6),
+        Edge(2, 3, 4),
+        Edge(3, 4, 3)
     ]
+    # peaks: int = 6 
+    # edges: int = 9 
+    # graph: List[Edge] = [
+    #     # example 2 from book
+    #     Edge(0, 1, 13),
+    #     Edge(0, 2, 18),
+    #     Edge(0, 3, 17),
+    #     Edge(0, 4, 14),
+    #     Edge(0, 5, 22),
+    #     Edge(1, 2, 26),
+    #     Edge(1, 4, 22),
+    #     Edge(2, 3, 3),
+    #     Edge(3, 5, 19)
+
+    # ]
     return peaks, edges, start_peack, graph
